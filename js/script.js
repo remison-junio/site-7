7512db800c97529052c021d7a965800008a5432d
Remison Junio <remisonpires@gmail.com>
2022-02-26 22:25:26 -0300

    Adicionando o DropDown do sub-menu

--- js/script.js
+++ js/script.js
@@ -8,11 +8,31 @@ const btnSubMenu = document.querySelectorAll('.btn-sub-menu');
 const subMenu = document.querySelectorAll('.sub-menu');
 
 btnSubMenu.forEach((item, indice)=> {
-	item.addEventListener('click', () => {
+	item.addEventListener('click', ()=> {
 		subMenu.forEach((item)=> {
 			item.classList.remove('active');
 		});
 
 		subMenu[indice].classList.add('active');
-	});
+	})
 });
+
+const navMenuDrop = document.querySelectorAll('.nav-menu-item-drop')
+console.log(navMenuDrop)
+
+navMenuDrop.forEach((item, indice)=> {
+	item.addEventListener('mouseenter', ()=> {
+		subMenu.forEach((item, indice)=> {
+			item.classList.remove('active');
+		});
+
+		subMenu[indice].classList.add('active');
+	})
+
+	item.addEventListener('mouseleave', ()=> {
+		subMenu.forEach((item, indice)=> {
+			item.classList.remove('active');
+		});
+		
+	})
+})
